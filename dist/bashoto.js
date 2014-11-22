11bc8a17fa9603d6fc4f061196e8c07b51556964
Buck Heroux <tmbuckheroux@gmail.com>
2014-11-22 14:50:05 -0500

    tests passing

--- dist/bashoto.js
+++ dist/bashoto.js
@@ -1,4 +1,4 @@
-/*! bashoto-js - v0.0.1 - 2014-11-13 - Buck Heroux */
+/*! bashoto-js - v0.0.2 - 2014-11-22 - Buck Heroux */
 ;(function (global) {
 
 // Compiler directive for UglifyJS.  See library.const.js for more info.
@@ -6,49 +6,16 @@ if (typeof DEBUG === 'undefined') {
     DEBUG = true;
 }
 
-
-// LIBRARY-GLOBAL CONSTANTS
-//
-// These constants are exposed to all library modules.
-
-
 // GLOBAL is a reference to the global Object.
 var Fn = Function, GLOBAL = new Fn('return this')();
 
-
-// LIBRARY-GLOBAL METHODS
-//
-// The methods here are exposed to all library modules.  Because all of the
-// source files are wrapped within a closure at build time, they are not
-// exposed globally in the distributable binaries.
-
-
 /*global console */
-/**
- * Init wrapper for the core module.
- * @param {Object} The Object that the library gets attached to in
- * library.init.js.  If the library was not loaded with an AMD loader such as
- * require.js, this is the global Object.
- */
 function initBashotoCore (context) {
-
 
     // It is recommended to use strict mode to help make mistakes easier to find.
     'use strict';
 
-
-    // PRIVATE MODULE CONSTANTS
-    //
-
-
-    // An example of a CONSTANT variable;
-
-
-    // PRIVATE MODULE METHODS
-    //
-    // These do not get attached to a prototype.  They are private utility
-    // functions.
-
+    // Format the errrors object properly for locate()
     function getErrorHandlers(errors) {
         var handlers = { error: function(error) { console.log(error); } };
         if (errors.position) {
@@ -72,12 +39,6 @@ function initBashotoCore (context) {
         return handlers;
     }
 
-    /**
-     * This is the constructor for the Bashoto Object.
-     * @param {Object} opt_config Contains any properties that should be used to
-     * configure this instance of the library.
-     * @constructor
-     */
     var Bashoto = context.Bashoto = function (app_key, opts) {
         opts = opts || {};
         this._appKey = app_key;
@@ -88,8 +49,7 @@ function initBashotoCore (context) {
         return this;
     };
 
-    Bashoto.HOST = "bashoto-core.herokuapp.com";
-    Bashoto.PROTOCOL = "wss://";
+    Bashoto.HOST = "https://bashoto.com";
 
     // Block - 100m
     Bashoto.HYPERLOCAL = Math.pow(10,2);
@@ -110,23 +70,12 @@ function initBashotoCore (context) {
     //
     // These methods define the public API.
 
-
-    /**
-     * An example of a protoype method.
-     * @return {string}
-     */
+    // Get the supplied application key
     Bashoto.prototype.getAppKey = function () {
         return this._appKey;
     };
 
-    Bashoto.prototype.setLocateErrorHandlers = function(handlers) {
-        this.handlers = getErrorHandlers(handlers);
-    };
-
-    /**
-     * An example of a protoype method.
-     * @return {string}
-     */
+    // Use HTML5 Geolocation to associate this object with a location
     Bashoto.prototype.locate = function(options) {
         var opts = options || {};
         var rng = opts.range || Bashoto.LOCAL;
@@ -179,27 +128,12 @@ function initBashotoCore (context) {
         });    
     };
 
-    // DEBUG CODE
-    //
-    // With compiler directives, you can wrap code in a conditional check to
-    // ensure that it does not get included in the compiled binaries.  This is
-    // useful for exposing certain properties and methods that are needed during
-    // development and testing, but should be private in the compiled binaries.
-
     if (DEBUG) {
         //GLOBAL.corePrivateMethod = corePrivateMethod;
     }
 
 }
 
-// Your library may have many modules.  How you organize the modules is up to
-// you, but generally speaking it's best if each module addresses a specific
-// concern.  No module should need to know about the implementation details of
-// any other module.
-
-// Note:  You must name this function something unique.  If you end up
-// copy/pasting this file, the last function defined will clobber the previous
-// one.
 /*global $:false */
 function initBashotoTopic (context) {
 
@@ -207,21 +141,11 @@ function initBashotoTopic (context) {
 
     var Bashoto = context.Bashoto;
 
-
-    // A library module can do two things to the Library Object:  It can extend
-    // the prototype to add more methods, and it can add static properties.  This
-    // is useful if your library needs helper methods.
-
-
-    // PRIVATE MODULE CONSTANTS
-    //
-
-
-    // PRIVATE MODULE METHODS
-    //
-
+    // No-op callback
     function noop() {}
 
+
+    // Get a formatted handlers obejct
     function getHandlers(hndlrs) {
         //TODO: Add debug and info
         var handlers = {
@@ -246,30 +170,15 @@ function initBashotoTopic (context) {
     }
 
     /**
-     * Get the url to connect to.  
-     */
-    function getSocketUrl(appKey, opts) {
-        var params = $.param(opts);
-        return Bashoto.PROTOCOL+Bashoto.HOST+"/io/topic/"+appKey+"?"+params;
-    }
-
-    // LIBRARY STATIC PROPERTIES
-    //
-
-
-    /**
-     * An example of a static Library property.  This particular static property
-     * is also an instantiable Object.
      * @constructor
      */
     var topic = Bashoto.Topic = function(appKey, handlers, opts) {
         opts = opts || {};
         handlers = handlers || {};
-        this._url = getSocketUrl(appKey, opts);
         this._handlers = getHandlers(handlers);
-        this._socket = this._bindsocket();
         this._openqueue = [];
         this._ons = {};
+        this._bindsocket(appKey, opts);
         return this;
     };
 
@@ -289,7 +198,7 @@ function initBashotoTopic (context) {
         if (typeof(msg) === "object") {
             msg = JSON.stringify(msg);
         }
-        if (this._socket.readyState === 0) {
+        if (!this.isOpen()) {
             this._openqueue.push(msg);
         } else {
             this._socket.send(msg);
@@ -297,45 +206,51 @@ function initBashotoTopic (context) {
     };
 
     topic.prototype.close = function() {
-        this._socket.close();
+        if (this.isOpen()) {
+            this._socket.close();
+        }
     };
 
     topic.prototype.isOpen = function() {
-        return this._socket.readyState === 1;
+        return this._socket && this._socket.readyState === 1;
     };
 
     // PRIVATE TOPIC METHODS
     //
 
-    topic.prototype._bindsocket = function() {
+    topic.prototype._bindsocket = function(appKey, opts) {
         var _topic = this;
-        var socket = new WebSocket(_topic._url);
-        //open
-        socket.onopen = function(msg) {
-            // flush openqueue
-            while(_topic._openqueue.length > 0) {
-                _topic.publish(_topic._openqueue.pop());
-            }
-            _topic._handlers.open(msg);
-        };
-        //close
-        socket.onclose = function(msg) {
-            _topic._handlers.close(msg);
-        };
-        //error
-        socket.onerror = function(msg) {
-            _topic._handlers.error(msg);
-        };
-        //message
-        socket.onmessage = function(msgevt) {
-            var message = JSON.parse(msgevt.data);
-            var msg = message.msg;
-            try {
-                msg = JSON.parse(message.msg);
-            } catch(e) { /*stays a string*/ }
-            _topic._handlers.message(msg);
-        };
-        return socket;
+        var url = Bashoto.HOST+"/io/topic/"+appKey+"?callback=?";
+        $.getJSON(url, function(response) {
+            var ws_url = response.response.url+"?"+$.param(opts); 
+            var socket = new WebSocket(ws_url);
+            //open
+            socket.onopen = function(msg) {
+                // flush openqueue
+                while(_topic._openqueue.length > 0) {
+                    _topic.publish(_topic._openqueue.pop());
+                }
+                _topic._handlers.open(msg);
+            };
+            //close
+            socket.onclose = function(msg) {
+                _topic._handlers.close(msg);
+            };
+            //error
+            socket.onerror = function(msg) {
+                _topic._handlers.error(msg);
+            };
+            //message
+            socket.onmessage = function(msgevt) {
+                var message = JSON.parse(msgevt.data);
+                var msg = message.msg;
+                try {
+                    msg = JSON.parse(message.msg);
+                } catch(e) { /*stays a string*/ }
+                _topic._handlers.message(msg);
+            };
+            _topic._socket = socket;
+        });
     };
 
     // BASHOTO PROTOTYPE METHODS
